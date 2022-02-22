39fffbec7a53a349431aff314e84d4d3317b22e4
Sakis Paligiannis <karafatmes@gmail.com>
2022-02-22 19:00:52 +0200

    Add files used in metric measurements.

---
+++ Experiments/TimeExecutionExperiments/src/main/java/time/execution/pipelines/DummyClass3.java
@@ -0,0 +1,1013 @@
+package time.execution.pipelines;
+
+public class DummyClass3 {
+
+	public DummyClass3() {
+		runDummies();
+	}
+	
+	
+	private void runDummies() {
+		dummyMethodFirst();
+		dummyMethodSecond();
+		dummyMethodThird();
+		dummyMethodFourth();
+		dummyMethodFiveth();
+		dummyMethodSixth();
+		dummyMethodSeventh();
+		dummyMethodEighth();
+		dummyMethodNineth();
+	}
+
+	private void dummyMethodFirst() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+	
+	private void dummyMethodSecond() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+	
+	private void dummyMethodThird() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+	
+	private void dummyMethodFourth() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+	
+	private void dummyMethodFiveth() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+	
+	private void dummyMethodSixth() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+
+	private void dummyMethodSeventh() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+	
+	private void dummyMethodEighth() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+
+	private void dummyMethodNineth() {
+
+		int a = 1;
+
+		a = a + 1;
+
+		int b = 2;
+
+		b = b + 2;
+
+		int c = 3;
+
+		c = c + 3;
+
+		int d = 4;
+
+		d = d + 4;
+
+		int e = 5;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+		e = e + 5;
+
+		d = c + b;
+
+		e = a + b;
+
+		a = b + d;
+
+	}
+
+
+}
